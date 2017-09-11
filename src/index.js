1688e23fa0fbd585d2d6a92222e571c70d2bd1b2
Abhishek <abhishek@satiri-389.guruestate.com>
2017-09-11 13:52:43 +0700

    os params read

---
+++ src/index.js
@@ -0,0 +1,23 @@
+'use strict'
+
+const os = require('os');
+const prettyMs = require('pretty-ms');
+const prettyBytes = require('pretty-bytes');
+
+function getOsStats() {
+	
+	const stats = {
+		cpus: os.cpus(),
+		freeMemory: prettyBytes(os.freemem()),
+		totalMemory: prettyBytes(os.totalmem()),
+		loadavg: os.loadavg(),
+		platform: os.platform(),
+		release: os.release(),
+		type: os.type(),
+		uptime: prettyMs(os.uptime() * 1000)
+	}
+	const statsString = 'OS Stats\n' + JSON.stringify(stats, null, 2);
+	return statsString;
+}
+
+console.log(getOsStats());
